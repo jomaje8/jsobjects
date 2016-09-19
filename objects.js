b95230e671f886503bff3d49eddae35e56b209d7
Jordan Jessee <jomaje8@gmail.com>
2016-09-19 11:48:29 -0600

    create html and add objects

--- objects.js
+++ objects.js
@@ -1,1 +1,27 @@
 // Objects lesson
+var Robot = {
+    fuel: 100,
+    weapon: "laser",
+    speed: 10,
+    stregnth: 5,
+    armor: "light",
+
+    AddFuel: function (tank){
+        fuel += tank;
+    },
+
+    UpgradeArmor: function (change){
+        armor = change
+    },
+
+    ChangeWeapon: function (change) {
+        weapon = change
+    },
+}
+
+Robot.UpgradeArmor();
+
+Robot.speed += parseInt(prompt("Increase Robot speed."));
+alert("Our robot now has a speed of " + Robot.speed + ".");
+Robot.UpgradeArmor(prompt("what should we upgrade the armor to?"))
+
